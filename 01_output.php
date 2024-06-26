de94790b89b7f5ea3ecdf006d4fe73214e50242c
Samir <122364498+Stileexx@users.noreply.github.com>
2024-06-26 21:08:06 +0200

    output and basic debugging

--- 01_output.php
+++ 01_output.php
@@ -1,9 +1,29 @@
 <?php
-echo 'Hello';
 
 /*
 dasdsa
 dsadas
-
 dssa
 */
+
+// used for debugging
+
+// echo [1, 2, 3];
+//echo - output strings, numbers, html, etc
+// echo 123, 'Hello', 10.5;
+
+/*     <?echo 'blabla';?>
+  OR
+/* <h1><?= 'example'; ?></h1>*/
+
+//print - like 'echo' but only 1 argument
+// print 423;
+
+// print_r() - print single values and arrays
+// print_r([1, 2, 3]);
+
+//var_dump() - returns more -> datatype and lenght
+// var_dump('letters');
+
+//var_export() - outputs string of a variable
+// var_export('Hello');
